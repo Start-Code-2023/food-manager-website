1f21320bc0502c38cb8a1a6a8f90ba20d25b7c35
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-27 19:19:34 +0200

    fix: change style to light mode

--- components/itemList.tsx
+++ components/itemList.tsx
@@ -8,12 +8,12 @@ interface Props{
 export default function ItemList({items}: Props){
 
     return (
-        <div className="w-96 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-96 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 ">
             <div className="flex items-center justify-between mb-4">
-                <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">In my fridge:</h5>
+                <h5 className="text-xl font-bold leading-none text-gray-900">In my fridge:</h5>
            </div>
            <div className="flow-root">
-                <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+                <ul role="list" className="divide-y divide-gray-700">
                    {items.map((item)=>{
                      return (<ItemComponent key={item.id} name={item.name} quantity={item.quantity} />)
                    })}
