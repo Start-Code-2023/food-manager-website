8b58fc47cbb98c073b7513c5a32e5eabf51537b9
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-27 18:16:09 +0200

    add: login page + tailwind config

---
+++ app/(site)/login/page.tsx
@@ -0,0 +1,12 @@
+import ItemList from '@/components/itemList'
+import { demoItems } from '@/constants/demo_items'
+
+export default function LoginScreen() {
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-mainGreen">
+        <h1 className="text-2xl font-bold">Login:</h1>
+    </main>
+  )
+}
