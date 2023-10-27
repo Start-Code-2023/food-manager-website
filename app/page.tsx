ac06737f1c75b0bed469e2df7be8831285741438
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-27 16:14:53 +0200

    add(component): list component for the items

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import ItemList from '@/components/itemList'
 import Image from 'next/image'
+import { demoItems } from '@/constants/demo_items'
 
 export default function Home() {
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="text-2xl font-bold">Food Manager</h1>
+
+
+        <h3>You have:</h3>
+
+        <div>
+          <ItemList items={demoItems} />
+        </div>
+
+
     </main>
   )
 }
---
+++ components/item.tsx
@@ -0,0 +1,25 @@
+interface Props{
+    name: string,
+    quantity: number
+    
+}
+
+export default function ItemComponent({name, quantity}: Props) {
+    return (
+        <li className="py-3 sm:py-4">
+            <div className="flex items-center space-x-4">
+                <div className="flex-1 min-w-0">
+                    <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        {name}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                        email@windster.com
+                    </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                    {quantity} unit(s). 
+                </div>
+            </div>
+        </li>
+    );
+}
