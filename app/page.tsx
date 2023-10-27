6b1b8048887a2b29d7a2d071f99022df7b4c158d
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-27 18:16:44 +0200

    refactor: main page changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
+import Button from '@/components/button'
 import ItemList from '@/components/itemList'
-import Image from 'next/image'
 import { demoItems } from '@/constants/demo_items'
 
 export default function Home() {
 
+  let signedIn = false;
+
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-2xl font-bold">Food Manager</h1>
-
-
-        <h3>You have:</h3>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-mainGreen">
+        <span className="text-5xl inline-flex"><h1 className="font-bold">food</h1> manager</span>
 
         <div>
-          <ItemList items={demoItems} />
+          {signedIn ? <Button link='/' text='View List'/> : <Button link='/login' text='Login'/>}
         </div>
-
 
     </main>
   )
