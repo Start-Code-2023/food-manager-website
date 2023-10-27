af6a937ad6c6aa5f917a0dcc01242dfa2db49632
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-27 15:49:23 +0200

    add: demo items added to the constants list

---
+++ constants/demo_items.ts
@@ -0,0 +1,53 @@
+export const demoItems: Item[] = [
+    {
+      id: "1",
+      name: "Apples",
+      quantity: 5,
+    },
+    {
+      id: "2",
+      name: "Bananas",
+      quantity: 8,
+    },
+    {
+      id: "3",
+      name: "Milk",
+      quantity: 2,
+    },
+    {
+      id: "4",
+      name: "Bread",
+      quantity: 1,
+    },
+    {
+      id: "5",
+      name: "Eggs",
+      quantity: 12,
+    },
+    {
+      id: "6",
+      name: "Pasta",
+      quantity: 3,
+    },
+    {
+      id: "7",
+      name: "Rice",
+      quantity: 4,
+    },
+    {
+      id: "8",
+      name: "Chicken",
+      quantity: 2,
+    },
+    {
+      id: "9",
+      name: "Cheese",
+      quantity: 1,
+    },
+    {
+      id: "10",
+      name: "Tomatoes",
+      quantity: 6,
+    }
+  ];
+  
